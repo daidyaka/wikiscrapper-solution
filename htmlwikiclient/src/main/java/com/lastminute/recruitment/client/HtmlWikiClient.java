fbfacc843879130d164787bc1e8ad1cf9453be83
Mykyta Trufanov <trufanov.n@outlook.com>
2023-06-15 23:45:35 +0200

    Fixing reading files due to "/" symbol replacing in file reading client classes. Added correct links to the html and json files, as the folder name they located in the classpath

--- htmlwikiclient/src/main/java/com/lastminute/recruitment/client/HtmlWikiClient.java
+++ htmlwikiclient/src/main/java/com/lastminute/recruitment/client/HtmlWikiClient.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class HtmlWikiClient {
 
     public String readHtml(String link) {
-        String name = link.replace("http://wikiscrapper.test/", "") + ".html";
+        String name = link.replace("http://wikiscrapper.test", "") + ".html";
         return Objects.requireNonNull(getClass().getResource(name)).getFile();
     }
 }
